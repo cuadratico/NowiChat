d326c39627a3855942828af334a154daedf1d4f5
Tirsi <tirsi333@gmail.com>
2025-06-02 19:10:53 +0200

    New gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.nowichat"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.1.3"
+        versionCode = 6
+        versionName = "0.1.3:1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
